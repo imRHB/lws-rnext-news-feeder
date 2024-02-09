700570f6516b24deceee6877a63c5f32afbd816d
Rakib Hasan Babu <rhbabu3@gmail.com>
2024-02-09 23:27:37 +0600

    init: :tada: news feeder initialized

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+    return (
+        <main>
+            <h1 className="text-5xl font-bold text-center text-violet-500">
+                News Feeder
+            </h1>
+        </main>
+    );
+}
+
+export default App;
